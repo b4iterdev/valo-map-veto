33dac0e99b33fcdac30f7ca77a793c6a765f8989
b4iterdev <thaimeo200389@gmail.com>
2025-02-04 17:40:32 +0700

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,8 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, valo-map-veto',
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, valo-map-veto');
   });
 });
